182fb85f1a1ac60e0312804595bc533f4d6ae51d
[kaky] <kasper.sebastian@gmail.com>
2021-05-12 13:25:00 +0200

    v1

--- ScrabbleTemplate/Program.fs
+++ ScrabbleTemplate/Program.fs
@@ -43,7 +43,7 @@ let main argv =
     let handSize   = 7u
     let timeout    = None
     let tiles      = ScrabbleUtil.English.tiles 1u
-    let seed       = None
+    let seed       = Some 420//None 
     let port       = 13001
 
     //let players = spawnMultiples "OxyphenButazone" Oxyphenbutazone.Scrabble.startGame 2
